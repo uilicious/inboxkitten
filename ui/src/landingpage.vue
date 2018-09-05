21c8403d810d0c9c69e195d8185536aaf426602b
jiaming <jiaming@uilicious.com>
2018-09-05 15:28:25 +0800

    Added plain landingpage

---
+++ ui/src/landingpage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <input type="text" :value="randomName"/>
+    </div>
+    <!-- Detail setup guide-->
+    <div>
+      <p>
+        Host your own InboxKitten!
+        - Deploy on ExpressJS
+        - Deploy using Google Lambda functions
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      randomName: ''
+    }
+  },
+  mounted () {
+    this.randomName = this.generateRandomName()
+  },
+  methods: {
+    generateRandomName () {
+      var randomWords = require('random-words')
+      return randomWords({exactly: 1, wordsPerString: 2, separator: '', formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1))})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
