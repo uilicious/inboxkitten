cc91394e197f1d98685ce87f45315856c6111142
jiaming <jiaming@uilicious.com>
2018-09-05 16:56:35 +0800

    Added scroll animation :D

--- ui/src/landingpage.vue
+++ ui/src/landingpage.vue
@@ -15,23 +15,25 @@
         <input type="text" :value="randomName" id="email-input"/> @inboxkitten.com <br>
         <button type="submit" class="pure-button pure-button-primary">Check inbox</button>
       </form>
-    </div>
-    <!-- Detail setup guide-->
-    <div>
       <div class="intermission-header">
         <p>Host your own InboxKitten!</p>
+        <i class="fa fa-chevron-down" @click="scrollDown"></i>
       </div>
-      <section>
-        <h2>Deploy on ExpressJS</h2>
-      </section>
-
-      <section><h2>Deploy using Google Lambda functions</h2></section>
-
     </div>
+    <!-- Detail setup guide-->
+    <section id="express-js">
+      <h2>Deploy on ExpressJS</h2>
+    </section>
+
+    <section id="google-js">
+      <h2>Deploy using Google Lambda functions</h2>
+    </section>
+
   </div>
 </template>
-
 <script>
+import $ from 'jquery'
+
 export default {
   name: 'LandingPage',
   data () {
@@ -46,7 +48,17 @@ export default {
   methods: {
     generateRandomName () {
       var randomWords = require('random-words')
-      return randomWords({exactly: 1, wordsPerString: 2, separator: '', formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1))})
+      return randomWords({
+        exactly: 1,
+        wordsPerString: 2,
+        separator: '',
+        formatter: (word) => word.slice(0, 1).toUpperCase().concat(word.slice(1))
+      })
+    },
+    scrollDown () {
+      $('html, body').animate({
+        scrollTop: $('#express-js').offset().top
+      }, 1000)
     }
   }
 }
@@ -55,28 +67,39 @@ export default {
 <style lang="scss" rel="stylesheet/scss">
 
   @import url("scss/_face.scss");
-  
-  #email-input{
+  @import url("https://use.fontawesome.com/releases/v5.3.1/css/all.css");
+
+  #email-input {
     border: none;
     border-color: transparent;
     text-align: right;
   }
 
-  .intermission-header{
-    height: 1vw
+  .intermission-header {
+    height: 2vh;
+    position: relative;
+    top: 40%;
   }
 
-  button{
-    margin-top:1rem;
+  button {
+    margin-top: 1rem;
   }
 
-  .main-section{
-    height: 30vw;
-    position:relative;
+  .main-section {
+    height: 100vh;
+    position: relative;
   }
 
-  .bottom-element{
+  .bottom-element {
     position: relative;
     top: 30%;
+  }
+
+  #express-js {
+    height: 50vh;
+  }
+
+  #google-js {
+    height: 50vh;
   }
 </style>
