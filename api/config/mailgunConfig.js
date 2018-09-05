87a5c5644c1bc7b3ae43fb8c38a74fa450054a14
Eugene Cheah <eugeneqin@gmail.com>
2018-09-05 15:32:39 +0800

    added mailgun api key

---
+++ api/config/mailgunConfig.js
@@ -0,0 +1,9 @@
+//
+// API key and valid mailgun domain supported (using sandbox)
+//
+// @TODO : Reset the API key and remove it from the repository - for obvious security reasons later, if opensourcing
+//
+module.exports = {
+	"api_key" : "48df1aaa2e15bee7319b799b790262a2-c1fe131e-f9a75615",
+	"domain"  : [ "sandboxa711e9753d1049ccbbba6b210b85bf02.mailgun.org" ]
+}
