9ec2e966551ec8301e8978faa5d0e33ad15bc783
Eugene Cheah <eugeneqin@gmail.com>
2018-09-06 09:35:50 +0800

    Updated mailgun key to "paid" account

--- api/config/mailgunConfig.js
+++ api/config/mailgunConfig.js
@@ -4,6 +4,6 @@
 // @TODO : Reset the API key and remove it from the repository - for obvious security reasons later, if opensourcing
 //
 module.exports = {
-	"apiKey" : "48df1aaa2e15bee7319b799b790262a2-c1fe131e-f9a75615",
+	"apiKey" : "c890691fc566dc8c1c2dd0b2cd190095-f45b080f-2b297bf8",
 	"emailDomain" : "test.popskitten.com"
 }
