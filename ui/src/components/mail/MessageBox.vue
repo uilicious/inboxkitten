91005a5d0b31bbb257fbc349f94d2c4e4105d716
jiaming <jiaming@uilicious.com>
2018-09-06 11:29:02 +0800

    Created message item box

---
+++ ui/src/components/mail/MessageBox.vue
@@ -0,0 +1,26 @@
+<template functional>
+  <div class="message-box">
+    <p>{{props.message.sender}}</p>
+    <p>{{props.message.subject}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  .message-box {
+    border: 1px solid lightgrey;
+  }
+  .message-box:hover {
+    box-shadow: 0px 10px 18px lightgrey;
+    overflow: hidden;
+  }
+</style>
