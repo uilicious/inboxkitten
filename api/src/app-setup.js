af01dd946d6a5947742f4977c76d39d0de423e06
Eugene Cheah <eugeneqin@gmail.com>
2018-09-07 19:28:58 +0800

    WIP firebase deployment steps

--- api/src/app-setup.js
+++ api/src/app-setup.js
@@ -1,16 +1,8 @@
-/**
- * This is configured for use within cloud function (or firebase cloud functions)
- *
- * See : https://firebase.google.com/docs/functions/http-events
- *
- * @TODO : AWS functions?
- */
 
 // Dependencies loading
-const functions     = require('firebase-functions');
 const express       = require("express");
 const bodyParser    = require("body-parser");
-const mailgunConfig = require("./config/mailgunConfig");
+const mailgunConfig = require("../config/mailgunConfig");
 
 // Initializing the express app
 const app = express();
@@ -36,13 +28,5 @@ app.use(function (req, res, next){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Setup the routes - for mail list / get
-app.get("/api/v1/mail/list",   require("./src/api/mailList"));
-app.get("/api/v1/mail/getUrl", require("./src/api/mailGetUrl"));
-app.get("/mail/list",   require("./src/api/mailList"));
-app.get("/mail/getUrl", require("./src/api/mailGetUrl"));
-app.get("/mail/getKey", require("./src/api/mailGetKey"));
-
-// Expose the HTTP on request
-exports.cloudfunction = functions.https.onRequest(app);
-exports.express = functions.https.onRequest(app);
+// Export the app module, for actual server deployment (on X)
+module.exports = app;
--- deploy/firebase/deploy.sh
+++ deploy/firebase/deploy.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
+# Deploy will terminate on any error
+set -e
+
 # Firebase Working directory
-workingDir="`dirname \"$0\"`"
-cd "$workingDir" || exit 1
-workingDir="`pwd`"
+firebaseDir="`dirname \"$0\"`"
+cd "$firebaseDir" || exit 1
+firebaseDir="`pwd`"
 
 # Project directory detection
-projectDir="$(cd "$workingDir/../.."; pwd)"
+projectDir="$(cd "$firebaseDir/../.."; pwd)"
 echo ">> Assuming project directory of : $projectDir"
 
-# Build the UI + CLI
-echo ">> Building the UI (NPM install + run build)"
-cd "$projectDir/ui"
-npm install;
-npm run build;
+# # Build the UI + CLI
+# echo ">> Building the UI (NPM install + run build)"
+# cd "$projectDir/ui"
+# npm install;
+# npm run build;
 
-# $(cd "$projectDir/ui"; npm install; npm run build)
-# $(cd "$projectDir/cli"; make build)
+# # Build the CLI
+# echo ">> Building the CLI"
+# cd "$projectDir/cli"
+# make build;
 
-# Copying over built files
+# Clearing out firebase public folder
+rm -rf "$firebaseDir/functions/"; 
+rm -rf "$firebaseDir/public/"; 
+mkdir -p "$firebaseDir/public/cli/";
+mkdir -p "$firebaseDir/functions/";
 
+# Transfering files into fire base deploy folder
+echo ">> Preparing build files for firebase upload"
+cp -r "$projectDir/ui/dist/" "$firebaseDir/public/"
+cp -r "$projectDir/cli/bin/" "$firebaseDir/public/cli/"
+cp -r "$projectDir/api/" "$firebaseDir/functions/"
+
+# Reconfigure the API function for firebase
+cp "$firebaseDir/functions/firebase.js" "$firebaseDir/functions/index.js"
+
+# Calling firebase deploy
+echo ">> Deploying to firebase"
+firebase deploy
