709f352c0f980397718efcc08692fff8f2c09fe8
Eugene Cheah <eugeneqin@gmail.com>
2018-09-07 18:54:01 +0800

    WIP build script

--- deploy/firebase/deploy.sh
+++ deploy/firebase/deploy.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Working directory
+# Firebase Working directory
 workingDir="`dirname \"$0\"`"
 cd "$workingDir" || exit 1
+workingDir="`pwd`"
+
+# Project directory detection
+projectDir="$(cd "$workingDir/../.."; pwd)"
+echo ">> Assuming project directory of : $projectDir"
+
+# Build the UI + CLI
+echo ">> Building the UI (NPM install + run build)"
+cd "$projectDir/ui"
+npm install;
+npm run build;
+
+# $(cd "$projectDir/ui"; npm install; npm run build)
+# $(cd "$projectDir/cli"; make build)
 
 # Copying over built files
+
