6bf33a77355c66f54f23e58da61c758f16f29b68
Eugene Cheah <eugeneqin@gmail.com>
2018-09-10 10:19:07 +0800

    Removing the mailgun security key - FOR SECURITY

---
+++ api/config/mailgunConfig.sample.js
@@ -0,0 +1,8 @@
+//
+// API key and valid mailgun domain supported (using sandbox)
+//
+module.exports = {
+	"apiKey" : "$APIKEY",
+	"emailDomain" : "$EMAIL_DOMAIN",
+	"corsOrigin" : "https://$EMAIL_DOMAIN"
+}
