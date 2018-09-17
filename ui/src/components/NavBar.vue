08250b44bc4787c98d2718ed0a5429a2311c4053
jiaming <jiaming@uilicious.com>
2018-09-17 19:55:09 +0800

    Changed to gradient color

--- ui/src/components/NavBar.vue
+++ ui/src/components/NavBar.vue
@@ -38,8 +38,10 @@
   $submit-box-width: 3rem;
   $box-height: 2rem;
 	.nav {
-		background-color: $color1-base;
-		width: 100vw;
+    background: #36D1DC;  /* fallback for old browsers */
+    background: -webkit-linear-gradient(to right, #5B86E5, #36D1DC);  /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(to right, #5B86E5, #36D1DC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    width: 100vw;
 		text-align: left;
     padding-top:2rem;
     padding-bottom:2rem;
