d893715b8e25a917771c06db9e87c4c1e2a5a971
jiaming <jiaming@uilicious.com>
2018-09-17 10:09:54 +0800

    responsive design for phone resolutions on landing page

--- ui/src/landingpage.vue
+++ ui/src/landingpage.vue
@@ -1,37 +1,38 @@
 <template>
   <div>
     <div class="main-section">
-
-      <div class="head">
-        <div class="ear ear-left"></div>
-        <div class="ear ear-edge-left"></div>
-        <div class="ear ear-right"></div>
-        <div class="ear ear-edge-right"></div>
-        <div class="face">
-          <div class="mouth"></div>
-          <div class="eye-group">
-            <div class="eye eye-left"></div>
-            <div class="eye eye-right"></div>
-          </div>
-        </div>
-      </div>
-      <form class="pure-form bottom-element">
-        <input type="text" v-model="randomName" id="email-input"/> @{{domain}} <br>
-        <button class="pure-button pure-button-primary inbox-button" @click="goToInbox">Check inbox</button>
+      <!--<div class="head">-->
+        <!--<div class="ear ear-left"></div>-->
+        <!--<div class="ear ear-edge-left"></div>-->
+        <!--<div class="ear ear-right"></div>-->
+        <!--<div class="ear ear-edge-right"></div>-->
+        <!--<div class="face">-->
+          <!--<div class="mouth"></div>-->
+          <!--<div class="eye-group">-->
+            <!--<div class="eye eye-left"></div>-->
+            <!--<div class="eye eye-right"></div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <img class="logo" src="assets/logo.png"/>
+      <form class="pure-form display-form">
+        <input type="text" v-model="randomName" id="email-input"/> <p>@{{domain}}</p>
       </form>
-      <div class="intermission-header">
-        <p>Host your own InboxKitten!</p>
-        <i class="fa fa-chevron-down" @click="scrollDown"></i>
-      </div>
+      <button class="pure-button pure-button-primary inbox-button" @click="goToInbox">Check inbox</button>
     </div>
-    <!-- Detail setup guide-->
-    <section id="express-js">
-      <h2>Deploy on ExpressJS</h2>
-    </section>
 
-    <section id="google-js">
-      <h2>Deploy using Google Lambda functions</h2>
-    </section>
+    <!--<div class="intermission-header">-->
+      <!--<p>Host your own InboxKitten!</p>-->
+      <!--<i class="fa fa-chevron-down" @click="scrollDown"></i>-->
+    <!--</div>-->
+    <!-- Detail setup guide-->
+    <!--<section id="express-js">-->
+      <!--<h2>Deploy on ExpressJS</h2>-->
+    <!--</section>-->
+
+    <!--<section id="google-js">-->
+      <!--<h2>Deploy using Google Lambda functions</h2>-->
+    <!--</section>-->
 
   </div>
 </template>
@@ -90,6 +91,7 @@ export default {
     border: none;
     border-color: transparent;
     text-align: right;
+    margin-right: 0.5rem;
   }
 
   .intermission-header {
@@ -98,32 +100,26 @@ export default {
     top: 40%;
   }
 
-  .inbox-button {
-    margin-top: 2rem;
+  @media only screen and (min-width:410px) {
+    .display-form {
+      display: flex;
+      flex-direction: row;
+    }
+  }
+
+  .display-form{
+    margin-top: 3rem;
+    margin-bottom: 1rem;
   }
 
   .main-section {
-    height: 100vh;
-    position: relative;
-  }
-
-  .bottom-element {
-    position: relative;
-    top: 30%;
-  }
-
-  #express-js {
-    height: 50vh;
-    background-color: lightgray;
-    padding-top:5rem;
+    height: 40vh;
+    width: 100vw;
+    position: absolute;
     display:flex;
     flex-direction: column;
     align-items: center;
-    padding-bottom: 5rem;
-  }
-
-  #google-js {
-    height: 50vh;
-    padding-top:5rem;
+    justify-content: center;
+    margin-top: 5rem;
   }
 </style>
