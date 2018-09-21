5941e5ab9ee6f6b9f4f1bc697b61cfbe679ab436
jiaming <jiaming@uilicious.com>
2018-09-21 18:39:13 +0800

    change to single quotes

--- ui/src/components/mail/message_list.vue
+++ ui/src/components/mail/message_list.vue
@@ -121,7 +121,7 @@ export default {
 
     formatName (sender) {
       let [name, emailUnformatted, ...rest] = sender.split(' <')
-      let [email, ...unknown] = emailUnformatted.split(">")
+      let [email, ...unknown] = emailUnformatted.split('>')
       return email
     },
 
