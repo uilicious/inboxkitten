3518dc8085119bf771e479ff97b8733b62321c25
jiaming <jiaming@uilicious.com>
2019-02-27 14:44:09 +0800

    Added endpoint to firebase configuration

--- api/firebase.js
+++ api/firebase.js
@@ -12,6 +12,7 @@ let app         = require("./src/app-setup");
 // Setup the routes - for mail list / get
 app.get("/api/v1/mail/list",   require("./src/api/mailList"));
 app.get("/api/v1/mail/getKey", require("./src/api/mailGetKey"));
+app.get("/api/v1/mail/getHtml", require("./src/api/mailGetHtml"));
 
 // Expose the HTTP on request
 exports.firebase_api_v1 = functions.https.onRequest(app);
