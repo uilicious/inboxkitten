c21fedf02b9d27b68a1905b397ae247fee3ee2ff
Eugene Cheah <eugeneqin@gmail.com>
2019-04-10 00:17:57 +0800

    Prototype KittenRouter implementation

--- api/src/cloudflare-api/KittenRouter.js
+++ api/src/cloudflare-api/KittenRouter.js
@@ -39,13 +39,20 @@ const exampleConfig = {
 		// Currently only elasticsearch is supported, scoped here for future alternatives
 		type : "elasticsearch",
 
+		//
 		// Elasticsearch index endpoint, note that a POST request will be done here
+		// the elasticsearch instance will then be expected to generate its own document id
+		// See : https://stackoverflow.com/questions/24756337/how-insert-data-to-elasticsearch-without-id
+		//
 		url : "https://user:password@elasticsearch-server.secret-domain.com/cluster/index",
 
-		// Mask the last digit of IPv4 address
+
+		// Enable logging of the full ipv4/6
+		//
+		// Else it mask (by default) the last digit of IPv4 address
 		// or the "network" routing for IPv6
 		// see : https://www.haproxy.com/blog/ip-masking-in-haproxy/
-		maskIP : true
+		logTrueIP : false
 	},
 
 	// routing rules to evaluate, starting from 0 index
@@ -64,6 +71,7 @@ const exampleConfig = {
 				// 	port : 443,
 				// 	protocol : "https",
 				// 	// Matching by country
+				// 	// Country codes : https://support.cloudflare.com/hc/en-us/articles/205072537-What-are-the-two-letter-country-codes-for-the-Access-Rules-
 				// 	country : [
 				// 		"SG"
 				// 	],
@@ -93,6 +101,108 @@ const exampleConfig = {
 //
 //---------------------------------------------------------------------------------------------
 
+// Simple regex to validate for an ipv4
+const ipv4_simpleRegex = /^[0-9a-z]{1,3}\.[0-9a-z]{1,3}\.[0-9a-z]{1,3}\.[0-9a-z]{1,3}$/i;
+
+// Extract from various possible options, a valid ipv4 from the request
+function getIPV4(request, logTrueIP = false) {
+	let headers = request.headers;
+	let ip = headers.get('cf-pseudo-ipv4') || headers.get('cf-connecting-ipv4') || headers.get('cf-connecting-ip') || '';
+
+	// If ipv4 validation failed, return blank
+	if(ip === '' || !ipv4_simpleRegex.test(ip)) {
+		return "";
+	}
+
+	// Assume that its a valid ipv4
+	// return immediately if no ipmasking is done
+	if(logTrueIP == false) {
+		return ip;
+	}
+
+	// Time to perform ip masking
+	let ip_split = ip.split(".");
+	ip_split[3] = "xxx";
+	return ip_split.join(".");
+}
+
+function getIPV6(request, logTrueIP = false) {
+	let headers = request.headers;
+	let ip = headers.get('cf-connecting-ipv6') || headers.get('cf-connecting-ip') || '';
+
+	// If ipv4 validation passes, return blank
+	if(ip === '' || ipv4_simpleRegex.test(ip)) {
+		return "";
+	}
+
+	// Assume that its an ipv6
+	// return immediately if no ipmasking is done
+	if(logTrueIP == false) {
+		return ip;
+	}
+
+	// Time to perform ip masking
+	let ip_split = ip.split(":");
+	for(let i=2; i<ip_split.length; ++i) {
+		ip_split[i] = "xxxx"
+	}
+	return ip_split.join(":");
+}
+
+async function logRequest(logConfig, request, response, routeType, routeCount) {
+	// Does nothing if logconfig is null
+	if( logConfig == null || logConfig.url == null || logConfig.url.length <= 0 ) {
+		return null;
+	}
+
+	// Trueip logging flag
+	let logTrueIP = logConfig.logTrueIP || false;
+
+	// The data to log
+	var data = {
+		'timestamp':  Date.now(),
+
+		'route.type' :  routeType,
+		'route.count' : routeCount,
+
+		'req.url':        request.url,
+		'req.referer':    request.referrer,
+		'req.method':     request.method,
+
+		'req.ipv4':          getIPV4(request, logTrueIP),
+		'req.ipv6':          getIPV6(request, logTrueIP),
+		'req.host':          request.headers.get('host') || '',
+		'req.user-agent':    request.headers.get('user-agent') || '',
+		'req.country-code':  request.headers.get('cf-ipcountry') || '',
+
+		'req.cf.ray':     request.headers.get('cf-ray') || '',
+		'req.cf.colo':    request.cf.colo,
+		'req.tlsVersion': request.cf.tlsVersion || '',
+		'req.tlsCipher':  request.cf.tlsCipher || '',
+
+		'res.status':           response.status,
+		'res.url':              response.url,
+		'res.server':           response.header.get('server') || '',
+		'res.via':              response.header.get('via') || '',
+		'res.content-type':     response.header.get('content-type') || '',
+		'res.content-encoding': response.header.get('content-encoding') || '',
+		'res.content-length':   response.header.get('content-length') || '',
+		'res.cache-control':    response.header.get('cache-control') || '',
+		'res.cf.cache-status':  response.header.get('cf-cache-status') || '',
+		'res.cf.ray':           response.header.get('cf-ray') || '',
+	};
+	
+	// The elasticsearch POST request to perform for logging
+	await fetch(
+		logConfig.url, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: new Headers({
+		  'Content-Type': 'application/json',
+		})
+	})
+}
+
 //---------------------------------------------------------------------------------------------
 //
 // Class implementation (and public interface)
@@ -113,11 +223,12 @@ class KittenRouter {
 	/**
 	 * Request handling function, and routing
 	 * 
-	 * @param  request from cloudflare to process
+	 * @param  event from cloudflare to process
 	 * 
 	 * @return response object for cloudflare
 	 */
-	handleRequest(request) {
-
+	await handleRequestEvent(event) {
+		// Get the request object
+		let request = event.request;
 	}
 }
