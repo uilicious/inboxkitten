5d3701a7cb6ad7a60787ae7a3fbca886f8a16761
daywalkers <k3yt07@gmail.com>
2024-06-22 11:27:52 +0700

    Delete api directory

--- api/config/mailgunConfig.sample.js
+++
@@ -1,8 +0,0 @@
-//
-// API key and valid mailgun domain supported (using sandbox)
-//
-module.exports = {
-	"apiKey"      : "${MAILGUN_API_KEY}",
-	"emailDomain" : "${MAILGUN_EMAIL_DOMAIN}",
-	//"corsOrigin"  : "*"
-}
--- api/webpack.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	optimization: {
-		// We no not want to minimize our code.
-		minimize: false
-	}
-}
